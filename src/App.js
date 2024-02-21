c8e34e31a186c73decbfd7d89f728b7cf16719ba
mislamfullstackdev <mislam.pasona@gmail.com>
2024-02-21 14:43:24 +0900

    React Video player App
    Used useState(), <video> tag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Menu from './components/Menu'
+import Video from "./components/Video";
+import "./style.css"
+
+const videos = {
+  deer:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4",
+  snail:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4",
+  cat:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4",
+  spider:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4"
+};
+
+const videoNames = Object.keys(videos)
 
 function App() {
+  const [videoSrc, setVideoSrc] = useState(videos.spider)
+
+  const onSelectVideo = (video) => {
+    const videoSrc = videos[video];
+    setVideoSrc(videoSrc)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Video player App</h1>
+      <Menu onSelectVideo={onSelectVideo} videoValues={videoNames} />
+      <Video videoSrc={videoSrc} />
     </div>
   );
 }
